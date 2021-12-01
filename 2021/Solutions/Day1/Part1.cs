3886bab00a09d8a036453e6ac0493bcc2c7527f2
Stian Trondsen <stiantrondsen@hotmail.com>
2021-12-01 17:17:56 +0100

    day 1 part 1

--- 2021/Solutions/Day1/Part1.cs
+++ 2021/Solutions/Day1/Part1.cs
@@ -1,13 +1,30 @@
-﻿namespace AoC2021.Solutions.Day1;
+﻿using System.Linq;
+
+namespace AoC2021.Solutions.Day1;
 
 public class Part1 : ISolutionPart
 {
     public string Name => "Part 1";
 
-    public string Description => "Calculate depth";
+    public string Description => "The number of times a depth measurement increases";
 
     public string Execute()
     {
-        return "Result!";
+        var input = Util.GetInput(1, "1.txt");
+
+        int? prev = null;
+        var count = 0;
+
+        foreach (var value in input)
+        {
+            var depth = int.Parse(value);
+
+            if (depth > prev)
+                count++;
+
+            prev = depth;
+        }
+
+        return count.ToString();
     }
 }
