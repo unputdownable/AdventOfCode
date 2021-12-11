586d8815991163c5d52ae057090a077824bfe872
Stian Trondsen <stiantrondsen@hotmail.com>
2021-12-11 23:28:44 +0100

    day 6 part 2 down to ~50MB memory from 14 gigs o_o

--- 2021/Solutions/Day6/Part2.cs
+++ 2021/Solutions/Day6/Part2.cs
@@ -14,25 +14,17 @@ public class Part2 : ISolutionPart
 
     public string Execute()
     {
-        var lanternfish = Util.GetInput(Day, "sample.txt").Single()
+        var ages = Util.GetInput(Day, "1.txt").Single()
             .Split(',')
-            .Select(x => int.Parse(x))
-            .ToList();
+            .Select(x => int.Parse(x));
 
-        for (int day = 0; day < 256; day++)
+        var simulator = new Simulator2(ages);
+
+        for (int i = 0; i < 256; i++)
         {
-            var count = lanternfish.Count;
-            for (int i = 0; i < count; i++)
-            {
-                lanternfish[i]--;
-                if (lanternfish[i] < 0)
-                {
-                    lanternfish.Add(8);
-                    lanternfish[i] = 6;
-                }
-            }
+            simulator.Step();
         }
 
-        return lanternfish.Count.ToString();
+        return simulator.Count.ToString();
     }
 }
---
+++ 2021/Solutions/Day6/Simulator2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2021.Solutions.Day6;
+
+class Simulator2
+{
+    long[] state = new long[9];
+
+    public Simulator2(IEnumerable<int> ages)
+    {
+        foreach (var age in ages)
+        {
+            state[age]++;
+        }
+    }
+
+    public long Count => state.Sum();
+
+    public void Step()
+    {
+        var spawnAmount = state[0];
+
+        state[0] = state[1];
+        state[1] = state[2];
+        state[2] = state[3];
+        state[3] = state[4];
+        state[4] = state[5];
+        state[5] = state[6];
+        state[6] = state[7] + spawnAmount;
+        state[7] = state[8];
+        state[8] = spawnAmount;
+    }
+}
